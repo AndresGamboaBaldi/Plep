ae5f5cee7abb93e8f1da6796d047840fd65bc0c9
Pedro Brañez <pedrobran8@gmail.com>
2021-05-09 17:28:17 -0400

    Update Movie object attributes

--- models/Movie.js
+++ models/Movie.js
@@ -4,7 +4,8 @@ const movieSchema = new Schema({
     title : {type : String, require:"true"},
     date: {type : Date, require:"true"},
     country: {type : String, require:"true"},
-    img:{ data: Buffer, contentType: String},
+    genre: {type : String, require:"true"},
+    img:{ type : String, require:"true"},
     url:{type : String, require:"true"}
 })
 module.exports = mongoose.model('movies', movieSchema)
