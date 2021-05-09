77f17ce4d65572411cc51ea62cb322cc0e14fde7
Pedro Brañez <pedrobran8@gmail.com>
2021-05-09 17:30:10 -0400

    Add description to Movie Object

--- models/Movie.js
+++ models/Movie.js
@@ -6,6 +6,7 @@ const movieSchema = new Schema({
     country: {type : String, require:"true"},
     genre: {type : String, require:"true"},
     img:{ type : String, require:"true"},
+    description:{ type : String, require:"true"},
     url:{type : String, require:"true"}
 })
 module.exports = mongoose.model('movies', movieSchema)
